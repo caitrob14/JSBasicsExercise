8f08ba5df5abb1759982067a7d3bd8c08b201ec0
Caitlin Robertson <robertson0630@gmail.com>
2021-09-20 16:45:16 -0500

    created script.js

---
+++ script.js
@@ -0,0 +1,31 @@
+//#1
+//console.log(14);
+
+//#2
+//Numbers, Strings, Booleans, Null, Undefined
+
+//#3
+//Adding, Subtracting, Multiplication, Division, Exponentiation, Modulus
+
+//#4
+//console.log(5%4);
+
+//#5
+//console.log('I\'m Mary Poppins, Y\'all!');
+
+//#6
+//console.log("Hawk " + "Eye");
+
+//#7
+//console.log("Caitlin Robertson".length);
+
+//#8
+//console.log("Avengers: Endgame" [16]);
+
+//B1
+//console.log("\\");
+
+//B2
+//console.log(5**4-9);
+
+console.log("I".length + "Am".length + "Iron".length + "Man".length -10);
